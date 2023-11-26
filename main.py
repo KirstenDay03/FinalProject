01770585f395f194c18ffee6fa05972d7b7c5dce
KirstenDay03 <113709467+KirstenDay03@users.noreply.github.com>
2023-11-26 16:26:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -87,3 +87,186 @@ async def update_recipe(id: int, recipe: Recipe):
 
 	return recipe
 
+
+@app.patch("/recipe/{id}/name")
+async def update_recipe_name(id: int, name: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['name']] = [name]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return name
+
+@app.patch("/recipe/{id}/cuisine")
+async def update_recipe_cuisine(id: int, cuisine: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['cuisine']] = [cuisine]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return cuisine
+
+@app.patch("/recipe/{id}/prepTime")
+async def update_recipe_prep_time(id: int, prepTime: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['prepTime']] = [prepTime]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return prepTime
+
+@app.patch("/recipe/{id}/cookTime")
+async def update_recipe_cook_time(id: int, cookTime: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['cookTime']] = [cookTime]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return cookTime
+
+@app.patch("/recipe/{id}/ingredient1")
+async def update_recipe_name(id: int, ingredient1: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['ingredient1']] = [ingredient1]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return ingredient1
+
+@app.patch("/recipe/{id}/ingredient2")
+async def update_recipe_name(id: int, ingredient2: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['ingredient2']] = [ingredient2]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return ingredient2
+
+@app.patch("/recipe/{id}/ingredient3")
+async def update_recipe_name(id: int, ingredient3: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['ingredient3']] = [ingredient3]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return ingredient3
+
+@app.patch("/recipe/{id}/ingredient4")
+async def update_recipe_name(id: int, ingredient4: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['ingredient4']] = [ingredient4]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return ingredient4
+
+@app.patch("/recipe/{id}/ingredient5")
+async def update_recipe_name(id: int, ingredient5: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['ingredient5']] = [ingredient5]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return ingredient5
+
+@app.patch("/recipe/{id}/instruction1")
+async def update_recipe_name(id: int, instruction1: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['instruction1']] = [instruction1]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return instruction1
+
+@app.patch("/recipe/{id}/instruction2")
+async def update_recipe_name(id: int, instruction2: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['instruction2']] = [instruction2]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return instruction2
+
+@app.patch("/recipe/{id}/instruction3")
+async def update_recipe_name(id: int, instruction3: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['instruction3']] = [instruction3]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return instruction3
+
+@app.patch("/recipe/{id}/instruction4")
+async def update_recipe_name(id: int, instruction4: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['instruction4']] = [instruction4]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return instruction4
+
+@app.patch("/recipe/{id}/instruction5")
+async def update_recipe_name(id: int, instruction5: str):
+	global df
+
+	if id not in df['id'].values:
+		raise HTTPException(status_code=404, detail="Recipe not found")
+
+	df.loc[df['id'] == id, ['instruction5']] = [instruction5]
+
+	df.to_csv('Recipe.csv', index=False)
+
+	return instruction5
+
